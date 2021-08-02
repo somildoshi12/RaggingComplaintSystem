dbb6ccfa929b72485c9a093ae13ff754a7d0fc9c
somildoshi12 <somildoshi12@gmail.com>
2021-08-02 22:51:17 +0530

    Third commit of 2/8/21

--- app/src/main/java/com/example/raggingcomplaintapp/MainActivity4.java
+++ app/src/main/java/com/example/raggingcomplaintapp/MainActivity4.java
@@ -2,22 +2,14 @@ package com.example.raggingcomplaintapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import java.util.Calendar;
-
 public class MainActivity4 extends AppCompatActivity {
-
-    EditText dob;
-    int day,month,year;
 
     RadioGroup radioGroup;
     RadioButton selectedRadioButton;
@@ -29,27 +21,6 @@ public class MainActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        dob = findViewById(R.id.editTextTextPersonName8);
-        Calendar calander = Calendar.getInstance();
-        dob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                day = calander.get(Calendar.DAY_OF_MONTH);
-                month = calander.get(Calendar.MONTH);
-                year = calander.get(Calendar.YEAR);
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity4.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        dob.setText(day + " / " + month + " / " + year);
-                    }
-                }, year, month, day);
-                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis() - 1000);
-                datePickerDialog.show();
-            }
-
-
-        });
         //For radio buttons
 
         radioGroup = findViewById(R.id.radioGroup);
