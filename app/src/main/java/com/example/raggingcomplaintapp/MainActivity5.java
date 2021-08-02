46756e14f8907523e41d2c5d5666eeb422886079
somildoshi12 <somildoshi12@gmail.com>
2021-08-03 02:38:33 +0530

    First commit of 3/8/21

--- app/src/main/java/com/example/raggingcomplaintapp/MainActivity5.java
+++ app/src/main/java/com/example/raggingcomplaintapp/MainActivity5.java
@@ -2,13 +2,26 @@ package com.example.raggingcomplaintapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity5 extends AppCompatActivity {
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        btn_login = findViewById(R.id.button5);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity5.this,MainActivity6.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/raggingcomplaintapp/MainActivity6.java
+++ app/src/main/java/com/example/raggingcomplaintapp/MainActivity6.java
@@ -2,13 +2,26 @@ package com.example.raggingcomplaintapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity6 extends AppCompatActivity {
+    Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+
+        btn_logout = findViewById(R.id.button6);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity6.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
